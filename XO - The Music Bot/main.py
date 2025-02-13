1429d9f86f3b3025ba372a59c039c5b74b11ea58
Omar Mustafa <omarmustafa130@gmail.com>
2025-02-13 22:23:07 +0200

    Initiated XO - The Music Bot

---
+++ XO - The Music Bot/main.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+from discord import Button, ButtonStyle
+
+client = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+
+#bot = ComponentsBot(command_prefix = )
