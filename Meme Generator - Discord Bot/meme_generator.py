24ce6d42af52e05f4e03b40ddc48dba0abf8f407
Omar Mustafa <omarmustafa130@gmail.com>
2025-02-14 02:11:58 +0200

    MemeGenerator Discord Bot Finished

--- Meme Generator - Discord Bot/meme_generator.py
+++ Meme Generator - Discord Bot/meme_generator.py
@@ -4,17 +4,6 @@ from dotenv import load_dotenv
 import requests
 import json
 
-#load env variables
-load_dotenv()
-
-TOKEN = os.getenv("DISCOR_BOT_TOKEN")
-
-print(TOKEN)
-
-intents = discord.Intents.default()
-intents.message_content=True
-
-client=discord.Client(intents=intents)
 
 def get_meme():
     url = 'https://meme-api.com/gimme'
@@ -23,3 +12,32 @@ def get_meme():
     subreddit = response['subreddit']
 
     return meme_large, subreddit
+
+
+#load env variables
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content=True
+
+client=discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    msg=message.content
+    if client.user == message.author:
+        return
+    if msg.lower().startswith('/meme'):
+        meme, subreddit = get_meme()
+        embed = discord.Embed(title='Meme', description=f'From r/{subreddit}', color=discord.Color.blue())
+        embed.set_image(url=meme)
+        await message.channel.send(embed=embed)
+
+client.run(TOKEN)
